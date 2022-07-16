0eda805f5c97e15cd3840605930e41f39e782f83
Prerna Makasare <makasareprerna@gmail.com>
2022-07-15 19:02:06 -0500

    Added Blind folder

---
+++ add_two_numbers.py
@@ -0,0 +1,28 @@
+from tkinter.tix import ListNoteBook
+
+
+def addTwoNumbers(l1, l2):
+        dummy = ListNoteBook()
+        cur = dummy
+        carry = 0
+        
+        while l1 or l2 or carry:
+            v1 = l1.val if l1 else 0
+            v2 = l2.val if l2 else 0
+            
+            #new digit
+            val = v1 + v2 + carry
+            carry = val // 10
+            val = val % 10
+            cur.next = ListNode(val)
+            
+            #Update ptrs
+            cur = cur.next
+            l1 = l1.next if l1 else None
+            l2 = l2.next if l2 else None
+        return dummy.next
+
+l1 = [2,4,3]
+l2 = [5,6,4]
+addTwoNumbers(l1, l2)
+        
